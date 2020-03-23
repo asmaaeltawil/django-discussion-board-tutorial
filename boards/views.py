51c959cd785ea942b3ea913901a1e6eedb2288fb
asmaaeltawil <semsema.eltawil@gmail.com>
2020-03-24 00:00:22 +0300

    Second Update by adding templates and static files

--- boards/views.py
+++ boards/views.py
@@ -4,10 +4,7 @@ from .models import Board
 # Create your views here.
 
 def home(request):
-    boards = Board.objects.all()
-    boards_names = []
-    for board in boards:
-        boards_names.append(board.name)
 
-    response_html = '<br>'.join(boards_names)
-    return HttpResponse(response_html)
+    boards = Board.objects.all()
+
+    return render(request,'home.html',{'boards':boards})
--- dicussion_board/settings.py
+++ dicussion_board/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dicussion_board.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static'),
+]
