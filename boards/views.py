a7902f3e3a38b762fa5e51967f75203692dd9524
asmaaeltawil <semsema.eltawil@gmail.com>
2020-04-01 01:32:53 +0300

    Second Update by adding templates and static files

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from django.http import HttpResponse,Http404
 from .models import Board
+from django.contrib.auth.models import User
+from .models import Topic,Post
 # Create your views here.
 
 def home(request):
@@ -17,6 +19,32 @@ def board_topics(request,board_id):
     #     raise Http404
     board = get_object_or_404(Board,pk=board_id)
     return render(request,'topics.html',{'board':board})
+
+
+def new_topic(request,board_id):
+    board = get_object_or_404(Board,pk=board_id)
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            created_by=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+        return redirect('board_topics',board_id=board.pk)
+    return render(request,'new_topic.html',{'board':board})
+
+
+
+
 
 
 
