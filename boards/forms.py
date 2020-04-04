7d8c2e169c69e6843fc78b42788c9c9ab4b6758f
asmaaeltawil <semsema.eltawil@gmail.com>
2020-04-05 01:17:32 +0300

    Update by using forms api

---
+++ boards/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Topic
+
+class NewTopicForm(forms.ModelForm):
+
+    message = forms.CharField(widget=forms.Textarea(
+        attrs={'rows':5,'placeholder':'What is on your mind?'}
+    ),
+    max_length=4000,
+    help_text='The max length od the text is 4000')
+
+    class Meta:
+        model = Topic
+        fields = ['subject','message']
