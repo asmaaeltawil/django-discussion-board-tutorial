45cefe711117e97000f95306a89f35b0d7ada11f
asmaaeltawil <semsema.eltawil@gmail.com>
2020-04-08 01:30:51 +0300

    Update by using forms api

--- accounts/url.py
+++ accounts/url.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('signup/',views.signup,name='signup'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout')
 
 ]
