2bd944b6c753e4ff14de1381379c8c987d8ccbdb
asmaaeltawil <semsema.eltawil@gmail.com>
2020-04-09 23:05:11 +0300

    Updated by authentication

--- accounts/url.py
+++ accounts/url.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/',views.signup,name='signup'),
-    path('logout/',auth_views.LogoutView.as_view(),name='logout')
+    path('logout/',auth_views.LogoutView.as_view(),name='logout'),
+    path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
 
 ]
