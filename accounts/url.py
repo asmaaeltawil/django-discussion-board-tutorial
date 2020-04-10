bda96427f1d0f044c3d8f77490418570b8987182
asmaaeltawil <semsema.eltawil@gmail.com>
2020-04-10 23:00:12 +0300

    Updated by authentication

--- accounts/url.py
+++ accounts/url.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('signup/',views.signup,name='signup'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
-
+    path('settings/change_password/',auth_views.PasswordChangeView.as_view(template_name='change_password.html'),name='password_change'),
+    path('settings/change_password/done/',auth_views.PasswordChangeDoneView.as_view(template_name='change_password_done.html'),name='password_change_done'),
 ]
